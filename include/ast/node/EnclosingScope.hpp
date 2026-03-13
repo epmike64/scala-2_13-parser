b4b95809a4f1c5c08cfdc5fba893b9b23b8e4a7b
epmike64 <epmike64@gmail.com>
2026-03-13 16:54:52 -0400

    editing

--- include/ast/node/EnclosingScope.hpp
+++ include/ast/node/EnclosingScope.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <map>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "EnclsScopeFwd.hpp"
@@ -10,11 +13,15 @@
 
 namespace zebra::ast::node {
 
+	class fLangValueDeclr {
+
+	};
 
 	class EnclosingScope {
 		const esc parentScope_;
 		const fLangGrmrProdE parentScopeGrmrProd_;
-		sp<std::vector<sp<fAstNod>>> polishCalcSS;  //Reverse Polish notation (RPN)
+		sp<std::vector<sp<fAstNod>>> polishCalcSS;
+		sp<std::unordered_map<std::string, sp<fLangValueDeclr>>> valueDeclrMap_;
 	public:
 		EnclosingScope(esc parentScope, fLangGrmrProdE parentScopeGrmrProd);
 		esc getParentScope() const;
@@ -24,6 +31,13 @@ namespace zebra::ast::node {
 		void setPolishCalcStack(sp<std::vector<sp<fAstNod>>>&& astRPN) ;
 
 		sp<std::vector<sp<fAstNod>>> getPolishCalcStack() const;
+
+		void addValueDeclr(const std::string& name, sp<fLangValueDeclr> declr) {
+			if (valueDeclrMap_ == nullptr) {
+				valueDeclrMap_ = util::ms<std::unordered_map<std::string, sp<fLangValueDeclr>>>();
+			}
+			(*valueDeclrMap_)[name] = declr;
+		}
 	};
 
 
