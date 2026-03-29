8253f38c00213e94685c3f32325bc84febd1df25
epmike64 <epmike64@gmail.com>
2026-03-28 22:41:11 -0400

    editing

--- src/back/tree/ZVisitTypeParamHelp.cpp
+++ src/back/tree/ZVisitTypeParamHelp.cpp
@@ -4,14 +4,10 @@
 #include <iostream>
 
 #include "ast/leaf/fClassParam.hpp"
-#include "ast/leaf/fClassParamClauses.hpp"
 #include "ast/leaf/fFunTypeParamClause.hpp"
-#include "ast/leaf/fParam.hpp"
-#include "ast/leaf/fParamType.hpp"
-#include "ast/leaf/fParamTypes.hpp"
 #include "ast/leaf/fTypeParamClause.hpp"
 #include "ast/symbol/ZEnclScope.hpp"
-#include "ast/symbol/ZLangConstruct.hpp"
+#include "back/tree/ZVisitUtil.hpp"
 
 namespace zebra::back::tree {
 	using namespace ast::symbol;
@@ -21,49 +17,42 @@ namespace zebra::back::tree {
 	void ZVisitTypeParamHelp::visitFunTypeParamClause(sp<fFunTypeParamClause> n, esc prnSc, sp<fAstNodVisitor> visitor) {
 		std::cout << "Visiting FunTypeParamClause: " << n->toString() << std::endl;
 
-		sp<ZTypeParamList> list = std::dynamic_pointer_cast<ZTypeParamList>(prnSc->getZSymbol());
-		if (!list) {
-			throw std::runtime_error("visitFunTypeParamClause: parent scope must contain a ZTypeParamList");
-		}
+		sp<ZTypeParamList> zDef = initScopeSymbol<ZTypeParamList>(prnSc);
 
 		for (auto typeParam: n->getTypeParamList()) {
-
-			sp<ZTypeParam> tp = ms<ZTypeParam>(typeParam->getIdentName());
-			esc tpSc = ms<ZEnclScope>(prnSc, tp);
-			typeParam->accept(visitor, tpSc);
-			list->addTypeParam(tp);
+			esc subSc = visitChildNode(typeParam, prnSc, visitor);
+			zDef->addTypeParam(dynSp<ZTypeParam>(subSc->getZSymbol()));
 		}
 	}
 
 	void ZVisitTypeParamHelp::visitTypeParamClause(sp<fTypeParamClause> n, esc prnSc, sp<fAstNodVisitor> visitor) {
+
+		sp<ZVariantTypeParamList> zDef = initScopeSymbol<ZVariantTypeParamList>(prnSc);
+
 		for (auto variantTypeParam: *n->getVariantTypeParams()) {
-			variantTypeParam->accept(visitor, prnSc);
+			esc subSc = visitChildNode(variantTypeParam, prnSc, visitor);
+			zDef->addVariantTypeParam(dynSp<ZVariantTypeParam>(subSc->getZSymbol()));
 		}
 	}
 
 	void ZVisitTypeParamHelp::visitVariantTypeParam(sp<fVariantTypeParam> n, esc prnSc, sp<fAstNodVisitor> visitor) {
 		std::cout << "Visiting VariantTypeParam: " << n->toString() << std::endl;
 
-		sp<ZVariantTypeParamList> list = dynSp<ZVariantTypeParamList>(prnSc->getZSymbol());
-		sp<ZVariantTypeParam> vtp = ms<ZVariantTypeParam>(n->getVariance());
-		list->addVariantTypeParam(vtp);
+		sp<ZVariantTypeParam> zDef = initScopeSymbol<ZVariantTypeParam>(prnSc, n->getVariance());
 
-		vtp->setTypeParam(ms<ZTypeParam>(n->getTypeParam()->getIdentName()));
-		esc tpScp = ms<ZEnclScope>(prnSc, vtp->getTypeParam());
-
-		n->getTypeParam()->accept(visitor, tpScp);
+		esc subSc = visitChildNode(n->getTypeParam(), prnSc, visitor);
+		zDef->setTypeParam(dynSp<ZTypeParam>(subSc->getZSymbol()));
 	}
 
 
 	void ZVisitTypeParamHelp::visitTypeParam(sp<fTypeParam> n, esc prnSc, sp<fAstNodVisitor> visitor) {
 		std::cout << "Visiting Type Parameter: " << n->getIdentToken()->toString() << std::endl;
 
-		sp<ZTypeParam> tp = dynSp<ZTypeParam>(prnSc->getZSymbol());
+		sp<ZTypeParam> zDef = initScopeSymbol<ZTypeParam>(prnSc, n->getIdentName());
 
 		if (n->getTypeParamClause()) {
-			tp->setVariantTypeParamList(ms<ZVariantTypeParamList>());
-			esc vtpListScp = ms<ZEnclScope>(prnSc, tp->getVariantTypeParamList());
-			n->getTypeParamClause()->accept(visitor, vtpListScp);
+			esc subSc = visitChildNode(n->getTypeParamClause(), prnSc, visitor);
+			zDef->setVariantTypeParamList(dynSp<ZVariantTypeParamList>(subSc->getZSymbol()));
 		}
 	}
 
